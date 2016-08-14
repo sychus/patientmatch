019aa9b44ac121828de62c5b9fe3c7f05e0b4452
hugo <hfernandez@neuquen.gov.ar>
2016-08-13 21:13:27 -0300

    Se mejora algoritmo con levenshtein distance

--- match.js
+++ match.js
@@ -1,5 +1,5 @@
 
-function maxLargo (stringAMin, stringBMin) {
+function maxLargo(stringAMin, stringBMin) {
 	var longA = stringAMin.length;
 	var longB = stringBMin.length;
 
@@ -10,7 +10,7 @@ function maxLargo (stringAMin, stringBMin) {
 	}
 }
 
-function minLargo (stringAMin, stringBMin) {
+function minLargo(stringAMin, stringBMin) {
 	var longA = stringAMin.length;
 	var longB = stringBMin.length;
 
@@ -27,7 +27,7 @@ function minLargo (stringAMin, stringBMin) {
  * @param {string} stringB - unString
  * @author Hugo Fernández hfernandez@neuquen.gov.ar 
  */
-function stringMatching (stringA, stringB) {
+function stringMatching(stringA, stringB) {
 	var stringAMin = stringA.toLowerCase();
 	var stringBMin = stringB.toLowerCase();
 
@@ -50,7 +50,7 @@ function stringMatching (stringA, stringB) {
  * @param {string} sexB - sexo del pacienteB
  * @author Hugo Fernández hfernandez@neuquen.gov.ar 
  */
-function sexMatching (sexA, sexB){
+function sexMatching(sexA, sexB) {
 	if (sexA == sexB)
 		return 1
 	else
@@ -63,7 +63,7 @@ function sexMatching (sexA, sexB){
  * @param {int} idB - Documento del pacienteB
  * @author Hugo Fernández hfernandez@neuquen.gov.ar 
  */
-function identityMatching (idA, idB){
+function identityMatching(idA, idB) {
 	if (idA == idB)
 		return 1
 	else
@@ -71,17 +71,58 @@ function identityMatching (idA, idB){
 
 }
 
+function levenshtein(stringA, stringB) {
+	var s1 = stringA.toLowerCase();
+	var s2 = stringB.toLowerCase();
+	var maxLen = maxLargo(s1, s2);
+
+	var l1 = s1.length;
+	var l2 = s2.length;
+	var d = [];
+	var c = 0;
+	var a = 0;
+
+	if (l1 == 0)
+		return l2;
+
+	if (l2 == 0)
+		return l1;
+
+	var d = new Buffer((l1 + 1) * (l2 + 1));
+	a = l1 + 1;
+
+	for (var i = 0; i <= l1; d[i] = i++);
+	for (var j = 0; j <= l2; d[j * a] = j++);
+
+	for (var i = 1; i <= l1; i++) {
+		for (var j = 1; j <= l2; j++) {
+			if (s1[i - 1] == s2[j - 1])
+				c = 0;
+			else
+				c = 1;
+			var r = d[j * a + i - 1] + 1;
+			var s = d[(j - 1) * a + i] + 1;
+			var t = d[(j - 1) * a + i - 1] + c;
+
+			d[j * a + i] = Math.min(Math.min(r, s), t);
+		}
+	}
+    
+	return  1 - ((d[l2 * a + l1])/maxLen) 
+}
+
+
 module.exports = {
-	matching: function(identidadA, identidadB, weights) {
+	matching: function (identidadA, identidadB, weights) {
 		var completeNameA = identidadA.name.given + identidadA.name.family;
 		var completeNameB = identidadB.name.given + identidadB.name.family;
-		
-		var value = ( weights.name * stringMatching (completeNameA, completeNameB) + 
-				 weights.gender * sexMatching (identidadA.gender, identidadB.gender) +
-				 weights.birthDate * stringMatching (identidadA.birthDate, identidadB.birthDate) + 
-				 weights.identity * identityMatching (identidadA.identity, identidadB.identity));
 
-		return value; 
+		var value = (weights.name * levenshtein(completeNameA, completeNameB) +
+			weights.gender * sexMatching(identidadA.gender, identidadB.gender) +
+			weights.birthDate * stringMatching(identidadA.birthDate, identidadB.birthDate) +
+			weights.identity * identityMatching(identidadA.identity, identidadB.identity));
+
+		return value;
 	}
 };
 
